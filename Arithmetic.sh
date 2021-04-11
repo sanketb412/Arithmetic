0d78bc49e261cb5ebdf71121e1f46acbec43c427
Shweta Singh <Shweta.Singh1@Nykaa.local>
2021-04-11 19:02:35 +0530

    Added: code for Equ1

--- Arithmetic.sh
+++ Arithmetic.sh
@@ -1,9 +1,13 @@
 
 
 echo "Welcome"
-
+#UC1
 read -p "Enter the 1st Number:- " a
 read -p "Enter the 2nd Number:- " b
 read -p "Enter the 3rd Number:- " c
 
-echo $a $b $c
+echo "The number Entered are '$a' '$b' and '$c' "
+
+#UC2
+P1=$((($a+$b)*$c))
+echo "The Solutiopn for equ1 is '$P1' "
